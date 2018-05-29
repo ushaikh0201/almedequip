4ca37c4de4ec4cf1adf1aa5c53042c65c0092cd4
ushaikh <ushaikh0201@gmail.com>
2018-05-29 06:03:31 +0530

    Try to fix error

--- server.js
+++ server.js
@@ -27,9 +27,10 @@ app.use(forceSSL());
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')) ) ;
 
 const PORT = process.env.PORT || 3000;
 
