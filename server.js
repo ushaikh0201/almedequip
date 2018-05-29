5663048889d4eb60eb711d1a9387407681a9ca7e
ushaikh <ushaikh0201@gmail.com>
2018-05-29 05:40:51 +0530

    Added IP

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ app.get('/*', function(req, res) {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is listening on port ${PORT}`);
 });
