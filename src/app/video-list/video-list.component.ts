788aaaabe4cb42e69d1aea69d4090c4ea9bc4d4e
ushaikh <ushaikh0201@gmail.com>
2018-06-05 06:48:43 +0530

    Video list improved

--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -12,4 +12,18 @@ export class VideoListComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngAfterViewInit(){
+    $(".arrow-right").bind("click", function (event) {
+      event.preventDefault();
+      $(".vid-list-container").stop().animate({
+          scrollLeft: "+=336"
+      }, 750);
+    });
+    $(".arrow-left").bind("click", function (event) {
+        event.preventDefault();
+        $(".vid-list-container").stop().animate({
+            scrollLeft: "-=336"
+        }, 750);
+    });
+  }
 }
